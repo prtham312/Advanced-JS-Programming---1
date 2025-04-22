ac1ede4de3aa1a62b5514d58b58286d35a7d8b5f
prtham312 <pb3488@srmist.edu.in>
2025-04-22 10:37:58 +0530

    Append a data in LL

--- linkedList.js
+++ linkedList.js
@@ -8,4 +8,25 @@ class linkedList{
     constructor(head){
         this.head = null;
     }
+    
+    
+    
+    //appending a data in linkedList
+    append(data){
+        const newNode = new Node(data);
+        if(!this.head){
+             this.head = newNode;
+        }
+        else{
+            let current = this.head 
+        while(current.next){
+            current = current.next;
+        }
+        current.next = newNode;
+    }
+    }
+
+
 }
+
+
