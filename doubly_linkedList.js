c70da63f3bb82106c514a365a5bde094c31fd1e2
prtham312 <pb3488@srmist.edu.in>
2025-04-22 12:18:47 +0530

    Implement class of DLL

---
+++ doubly_linkedList.js
@@ -0,0 +1,15 @@
+class Node{
+    constructor(data){
+        this.data = data;
+        this.prev = null;
+        this.next = null;
+    }
+}
+class doublyLinkedList{
+    constructor(){
+        this.head = null;
+        this.tail = null;
+        this.lenght = 0;
+    }
+
+}
