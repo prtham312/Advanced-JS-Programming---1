4302af47c4c590be1636d6a91e296a4a32ddaf8b
prtham312 <pb3488@srmist.edu.in>
2025-04-22 12:28:41 +0530

    inserting a data in DLL

--- doubly_linkedList.js
+++ doubly_linkedList.js
@@ -9,7 +9,21 @@ class doublyLinkedList{
     constructor(){
         this.head = null;
         this.tail = null;
-        this.lenght = 0;
+        this.length = 0;
     }
 
+    insert(data){ //inserting a data in DLL
+        const newNode = new Node(data);
+        if(!this.head){
+            this.head = newNode;
+            this.tail = newNode;
+        }
+        else{
+          newNode.next = this.head;
+          this.head.prev = newNode;
+          this.head = newNode;  
+        }
+        this.length++;
+    }
+    
 }
