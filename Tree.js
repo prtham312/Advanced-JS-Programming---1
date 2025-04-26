276585677264d9d8cf43a0b5abf5e99a49f71afc
prtham312 <pb3488@srmist.edu.in>
2025-04-26 11:28:56 +0530

    BFS in trees

--- Tree.js
+++ Tree.js
@@ -9,3 +9,23 @@ let root = new TreeNode(1);
 root.left = new TreeNode(2);
 root.right = new TreeNode(3);
 console.log(root);
+
+
+//BFS
+function bfs(root){
+
+    if(root==null) return;
+
+    const queue = [root];
+    while(queue.length){
+        const item = queue.shift();
+        console.log(item);
+
+        if(item.left) queue.push(item.left)
+        if(item.right) queue.push(item.right)
+    }
+    
+
+}
+
+bfs(root);
