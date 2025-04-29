4466420f99103d609809420eb00429c98c4a6170
prtham312 <pb3488@srmist.edu.in>
2025-04-29 14:42:59 +0530

    Object.defineProperty()

--- Object.js
+++ Object.js
@@ -1,3 +1,10 @@
 const bird = {}
 bird.name = "HummingBird" //dot notation property add
 bird["canFly"] = true;
+Object.defineProperty(bird , 'live' , {
+    value : 25 ,
+    writable : true,
+    configurable : true ,
+    enumerable : true
+})
+console.log(bird.live)
