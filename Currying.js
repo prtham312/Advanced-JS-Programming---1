f9b07bfe930c97cc53e398150fe1db3116091f58
prtham312 <pb3488@srmist.edu.in>
2025-05-05 12:51:49 +0530

    Partial Currying

--- Currying.js
+++ Currying.js
@@ -10,3 +10,9 @@ console.log(add(2,3,4))
 
 const addCurry = a => b => c => a+b+c;
 console.log(addCurry(2)(3)(5))
+
+
+{
+const add = a => b => c => a + b + c;
+const add5 = add(2)(3); //this will not print here anything as it is expecting a value here
+console.log(add5(10)); }
